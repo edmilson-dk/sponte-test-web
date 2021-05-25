5e3260d8e28e6ec253fbabf915238ff7f5a74643
edmilson-dk <droidvidaboa@gmail.com>
2021-05-25 08:15:53 -0300

    style: created default custom theme

---
+++ src/styles/index.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  primaryLight: "#0198DB",
+  primary: "#0172B7",
+  greenDark: "#2DA537",
+  greenLight: "#3EC13E",
+  danger: "#DB261F",
+  warning: "#FFC10A",
+  bgPrimary: "#fff",
+  bgSecond: "rgba(244, 244, 244, 0.8)"
+}
+
+type CustomThemeProviderProps = {
+  children: ReactNode;
+}
+
+export function CustomThemeProvider({ children }: CustomThemeProviderProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      { children }
+    </ThemeProvider>
+  )
+}
