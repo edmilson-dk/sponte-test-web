8f5c223e987dfce4a605fd069df5d90b72b87e04
edmilson-dk <droidvidaboa@gmail.com>
2021-05-25 12:00:48 -0300

    feat: created product api types

--- src/services/api/product-api/types.ts
+++ src/services/api/product-api/types.ts
@@ -11,3 +11,23 @@ export type ProductData = {
   acquisition: Date;
   image: string;
 }
+
+export type ProductStoreType = {
+  title: string;
+  description: string;
+  height: number;
+  width: number;
+  length: number;
+  weight: number;
+  barCode: string;
+  category: string[];
+  value: number;
+  acquisition: Date;
+  image: string;
+  id: string;
+}
+
+export type ProductsStoreType = {
+  data: ProductStoreType[],
+  count: number;
+}
