f2a6993f193cf75f207b45999709cf09b4fcf8dd
edmilson-dk <droidvidaboa@gmail.com>
2021-05-25 19:49:45 -0300

    feat: added product events data

--- src/screens/dashboard-home/index.tsx
+++ src/screens/dashboard-home/index.tsx
@@ -1,8 +1,11 @@
 import { DashboardBoxInfo } from "src/components/dashboard-box-info";
+import { useProductsContext } from "src/contexts/products-context";
 import { Container } from "src/styles/components/container";
 import { DashboardHomeHeader, DashboardHomeWrapper } from "src/styles/screens/dashboard-home";
+import { formatDate } from "src/utils/formatDate";
 
 export function DashboardHome() {
+  const {  deletedCount, createdCount, updatedCount } = useProductsContext();
 
   return (
     <DashboardHomeWrapper>
@@ -10,18 +13,18 @@ export function DashboardHome() {
         <DashboardHomeHeader>
           <DashboardBoxInfo 
             title="Total" 
-            value={10} 
-            updatedAt="10 Mar 20" 
+            value={createdCount.count ?? 0} 
+            updatedAt={ createdCount.count !== undefined ? formatDate(createdCount.updatedDateCount) : "Loading"} 
             bgType="created"/>
           <DashboardBoxInfo 
             title="Deletados" 
-            value={2} 
-            updatedAt="10 Mar 20" 
+            value={deletedCount.deletedCount ?? 0} 
+            updatedAt={ deletedCount.deletedCount !== undefined ? formatDate(deletedCount.updatedDateDaletedCount) : "Loading"}
             bgType="deleted"/>
           <DashboardBoxInfo 
             title="Atualizados" 
-            value={4} 
-            updatedAt="10 Mar 20" 
+            value={updatedCount.updatedCount ?? 0} 
+            updatedAt={ updatedCount.updatedCount !== undefined ? formatDate(updatedCount.updatedDateUpdatedCount) : "Loading"}
             bgType="updated"/>
         </DashboardHomeHeader>
       </Container>
