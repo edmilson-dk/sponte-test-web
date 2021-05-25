207cb35cf86d975445bc15559c807c00c7e07766
edmilson-dk <droidvidaboa@gmail.com>
2021-05-25 19:48:41 -0300

    feat: created delete product api method

--- src/services/api/product-api/index.ts
+++ src/services/api/product-api/index.ts
@@ -1,31 +1,60 @@
 import { v4 } from "uuid";
 
 import { constants } from "src/constants";
-import { ProductData, ProductsStoreType } from "./types";
+import { ProductData, ProductEventsStoreData, ProductEventsStoreType, ProductsStoreType } from "./types";
 
 export class ProductApi {
+
+  private addProductEvent(data: ProductEventsStoreData) {
+    window.localStorage.setItem(
+      constants.storagedProducEventsKey,
+      JSON.stringify(data));
+
+    return;
+  }
+
+  private updateProductEventCount(newEventValue: object, oldData: ProductEventsStoreType) {
+    window.localStorage.setItem(
+      constants.storagedProducEventsKey, 
+      JSON.stringify({ ...oldData, ...newEventValue }));
+
+    return;
+  }
+
   addProduct(data: ProductData) {
     const id = v4();
-    const updatedAt = new Date;
-    const createdAt = new Date;
+    const updatedAt = new Date();
+    const createdAt = new Date();
     const newProduct = { ...data, id, updatedAt, createdAt };
 
     let allProducts: ProductsStoreType | null = this.getProducts();
+    let allProductsEvents: ProductEventsStoreType | null = this.getProductsEvents();
 
-    if (!allProducts) {
-      allProducts = {
-        data: [newProduct],
-        count: 1,
-        deletedCount: 0,
-        updatedCount: 0,
-      }
-    } else {
-      allProducts.data.push(newProduct);
-      allProducts.count = allProducts.data.length;
-    }
+    !allProducts 
+      ? allProducts = { data: [newProduct] }
+      : allProducts.data.push(newProduct);
 
+    !allProductsEvents 
+      ? this.addProductEvent({ 
+        count: 1, deletedCount: 0, updatedCount: 0,   
+        updatedDateCount: new Date(),
+        updatedDateDeletedCount: new Date(),
+        updatedDateUpdatedCount: new Date(), })
+      : this.updateProductEventCount({ 
+        count: allProducts.data.length,
+        updatedDateCount: new Date(),
+      }, allProductsEvents);
+    
     window.localStorage.setItem(constants.storagedProductKey, JSON.stringify(allProducts));
     return;
+  }
+
+  getProductsEvents(): ProductEventsStoreType | null {
+    const allProductsEvents = window.localStorage.getItem(constants.storagedProducEventsKey);
+
+    if (!allProductsEvents) return null;
+    
+    return JSON.parse(allProductsEvents);
   }
 
   getProducts(): ProductsStoreType | null {
@@ -34,5 +63,24 @@ export class ProductApi {
     if (!allProducts) return null;
     
     return JSON.parse(allProducts);
+  }
+
+  deleteProduct(id: string) {
+    const allProducts = this.getProducts();
+    const allProductsEvents = this.getProductsEvents();
+
+    if (!allProductsEvents || !allProducts) return;
+
+    console.log(allProducts)
+    const newProducts = allProducts.data.filter(product => product.id !== id);
+    
+    this.updateProductEventCount({ 
+      deletedCount: allProductsEvents.deletedCount +1,
+      updatedDateDeletedCount: new Date(),
+     }, allProductsEvents);
+
+    window.localStorage.setItem(constants.storagedProductKey, JSON.stringify({ data: newProducts }));
+
+    return;
   }
 }
