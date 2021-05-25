206b56b4d81e7bf204f14276338a3beefef249b7
edmilson-dk <droidvidaboa@gmail.com>
2021-05-25 19:49:02 -0300

    feat: added news types

--- src/services/api/product-api/types.ts
+++ src/services/api/product-api/types.ts
@@ -29,9 +29,30 @@ export type ProductStoreType = {
   id: string;
 }
 
-export type ProductsStoreType = {
-  data: ProductStoreType[],
+export type ProductEventsType = {
   count: number;
   deletedCount: number;
   updatedCount: number;
+}
+
+export type ProductEventsStoreType = {
+  count: number;
+  updatedDateCount: string;
+  deletedCount: number;
+  updatedDateDeletedCount: string;
+  updatedCount: number;
+  updatedDateUpdatedCount: string;
+}
+
+export type ProductEventsStoreData = {
+  count: number;
+  updatedDateCount: Date;
+  deletedCount: number;
+  updatedDateDeletedCount: Date;
+  updatedCount: number;
+  updatedDateUpdatedCount: Date;
+}
+
+export type ProductsStoreType = {
+  data: ProductStoreType[];
 }
