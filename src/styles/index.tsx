605f3f009b68c27e6b9d81cb4a49654f271a7d1b
edmilson-dk <droidvidaboa@gmail.com>
2021-05-25 15:32:28 -0300

    feat: added new theme styles object with colors

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -3,20 +3,29 @@ import { ThemeProvider } from "styled-components";
 
 const theme = {
   colors: {
-    primaryLight: "#45BAF9",
-    primary: "#3292D6",
-    blueDark: "#091A7A",
+    primaryLight: "#45baf9",
+    primary: "#3292d6",
+    blueDark: "#091a7a",
     dark: "#0D2534",
     greenDark: "#2DA537",
-    greenLight: "#3EC13E",
-    danger: "#DB261F",
+    greenLight: "#3ec13e",
+    danger: "#db261f",
     warning: "#FFC10A",
     white: "#fff",
     whiteSecond: "rgba(244, 244, 244, 0.8)",
+    gray200: "#AFB2B1",
+    gray500: "#808080",
+    gray800: "#494D4B",
   },
   utility: {
     radius: "5px",
   }
+}
+
+export const colorsSwitch = {
+  created: theme.colors.greenLight,
+  updated: theme.colors.primaryLight,
+  deleted: theme.colors.danger
 }
 
 type CustomThemeProviderProps = {
