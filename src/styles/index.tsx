92724f908dc46a234edf14935c6caec7bc7ede5b
edmilson-dk <droidvidaboa@gmail.com>
2021-05-25 10:44:03 -0300

    style: added news colors

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -2,15 +2,21 @@ import { ReactNode } from "react";
 import { ThemeProvider } from "styled-components";
 
 const theme = {
-  primaryLight: "#0198DB",
-  primary: "#0172B7",
-  greenDark: "#2DA537",
-  greenLight: "#3EC13E",
-  danger: "#DB261F",
-  warning: "#FFC10A",
-  bgPrimary: "#fff",
-  bgSecond: "rgba(244, 244, 244, 0.8)",
-  radius: "5px",
+  colors: {
+    primaryLight: "#45BAF9",
+    primary: "#3292D6",
+    blueDark: "#091A7A",
+    dark: "#0D2534",
+    greenDark: "#2DA537",
+    greenLight: "#3EC13E",
+    danger: "#DB261F",
+    warning: "#FFC10A",
+    white: "#fff",
+    whiteSecond: "rgba(244, 244, 244, 0.8)",
+  },
+  utility: {
+    radius: "5px",
+  }
 }
 
 type CustomThemeProviderProps = {
