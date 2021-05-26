004cc7cc84730a2abf7aadfdbce28ad4e94545b2
edmilson-dk <droidvidaboa@gmail.com>
2021-05-26 12:14:56 -0300

    feat: created new format date function

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -4,3 +4,7 @@ import { ptBR } from "date-fns/locale";
 export function formatDate(date: string) {
   return format(parseISO(date), 'd MMM yy', { locale: ptBR });
 }
+
+export function formatDateToCalendar(date: string) {
+  return format(parseISO(date), 'd MM yy', { locale: ptBR });
+}
