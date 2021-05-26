c7e77b4dcb35e40b64c9e228a7c7d58383a10355
edmilson-dk <droidvidaboa@gmail.com>
2021-05-26 14:10:17 -0300

    style: added input and textarea focus effect

--- src/styles/components/dashboard-product-form/input/index.ts
+++ src/styles/components/dashboard-product-form/input/index.ts
@@ -15,34 +15,32 @@ export const ProductInputWrapper = styled.div`
     margin-bottom: 10px;
   }
 
-  > input {
-    height: 50px;
-    padding: 0 1rem;
+  > input,
+  > textarea {
     width: 100%;
-
     border-radius: ${({ theme }) => theme.utility.radius};
     background-color: ${({ theme }) => theme.colors.whiteSecond};
-
+    border: 1px solid ${({ theme }) => theme.colors.gray100};
     color: ${({ theme }) => theme.colors.gray500};
     font-size: 18px;
     font-weight: 400;
     box-shadow: 3px 3px 3px rgba(0,0,0,0.060);
+
+    &:focus {
+      border-color: ${({ theme }) => theme.colors.primary};
+      border-width: 2px;
+    }
+  }
+
+  > input {
+    height: 50px;
+    padding: 0 1rem;
   }
 
   > textarea {
     height: 100px;
-    width: 100%;
     max-height: 300px;
     resize: vertical;
     padding: 1rem;
-
-    border: none;
-    border-radius: ${({ theme }) => theme.utility.radius};
-    background-color: ${({ theme }) => theme.colors.whiteSecond};
-
-    color: ${({ theme }) => theme.colors.gray500};
-    font-size: 18px;
-    font-weight: 500;
-    box-shadow: 3px 3px 3px rgba(0,0,0,0.060);
   }
 `;
