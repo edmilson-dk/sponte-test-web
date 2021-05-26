d32d0b3518ce148e2d29b6f94a8b5f9265d3d721
edmilson-dk <droidvidaboa@gmail.com>
2021-05-26 20:28:17 -0300

    feat: added dashboard update page router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { DashboarWrapper } from "src/components/dashboard-wrapper";
 import { DashboardCreate } from "src/screens/dashboard-create";
 import { DashboardHome } from "src/screens/dashboard-home";
+import { DashboardUpdate } from "src/screens/dashboard-update";
 
 export function Routes() {
   return (
@@ -11,6 +12,7 @@ export function Routes() {
         <DashboarWrapper>
           <Route exact path="/"component={DashboardHome}/>
           <Route path="/new" component={DashboardCreate}/>
+          <Route path="/update" component={DashboardUpdate}/>
         </DashboarWrapper>
       </Switch>
     </BrowserRouter>
