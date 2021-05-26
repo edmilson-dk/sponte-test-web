d7ca44223c4119095c6067777e9e1946fab0633c
edmilson-dk <droidvidaboa@gmail.com>
2021-05-25 22:07:53 -0300

    feat: created dashboard product box

---
+++ src/components/dashboard-product-box/index.tsx
@@ -0,0 +1,47 @@
+import { FiEye, FiSettings, FiTrash2 } from "react-icons/fi";
+import { DashboardProductBoxWrapper } from "src/styles/components/dashboard-product-box";
+
+import { DashboardProductBoxProps } from "./types";
+
+export function DashboardProductBox({ title, category, image, value, id }: DashboardProductBoxProps) {
+  return (
+    <DashboardProductBoxWrapper>
+      <div>
+        <img src={image} alt="Produto infos" />
+      </div>
+      <article>
+        <p>
+          <strong>Titúlo:</strong>
+          <span>{title}</span>
+        </p>
+        <p>
+          <strong>Valor:</strong>
+          <span>{value}</span>
+        </p>
+        <p id="product-category">
+          <strong>Catégorias:</strong>
+          {
+            category.map(c => <span key={c}>{c}</span>)
+          }
+        </p>
+      </article>
+      <footer>
+        <button id="view-product">
+          <span>
+            <FiEye size="100%"/>
+          </span>
+        </button>
+        <button id="update-product">
+          <span>
+            <FiSettings size="100%"/>
+          </span>
+        </button>
+        <button id="delete-product">
+          <span>
+            <FiTrash2 size="100%"/>
+          </span>
+        </button>
+      </footer>
+    </DashboardProductBoxWrapper>
+  )
+}
---
+++ src/components/dashboard-product-box/types.ts
@@ -0,0 +1,8 @@
+export type DashboardProductBoxProps = {
+  title: string;
+  category: string[],
+  value: number;
+  image: string;
+  createdAt: string;
+  id: string;
+}
