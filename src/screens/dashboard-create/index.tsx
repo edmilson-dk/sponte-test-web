d72819b77f90db3f4cf9d62a8d113759d8f7de37
edmilson-dk <droidvidaboa@gmail.com>
2021-05-26 12:13:35 -0300

    feat: created created new product dashboard page

---
+++ src/screens/dashboard-create/index.tsx
@@ -0,0 +1,9 @@
+import { DashboardProductForm } from "src/components/dashboard-product-form";
+
+export function DashboardCreate() {
+  return (
+    <>
+      <DashboardProductForm />
+    </>
+  )
+}
