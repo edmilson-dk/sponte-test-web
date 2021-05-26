9d6525bb32e87652d393ec219f2cdab5978f0f5e
edmilson-dk <droidvidaboa@gmail.com>
2021-05-26 19:55:10 -0300

    feat: added dashboard product modal

--- src/components/dashboard-product-form/index.tsx
+++ src/components/dashboard-product-form/index.tsx
@@ -1,5 +1,5 @@
 import { useFormik } from "formik";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 import { constants } from "src/constants";
 import { ProductData } from "src/services/api/product-api/types";
@@ -9,9 +9,11 @@ import { ProductValidateSchema, validate } from "src/services/product-validate";
 import { ProductMultipleInputValue } from "./multiple-input-values";
 import { formatCategories } from "src/utils/formatCategories";
 import { useProductsContext } from "src/contexts/products-context";
+import { DashboardProductModal } from "../dashboard-product-modal";
 
 export function DashboardProductForm() {
   const [ category, setCategory ] = useState([{ key: "", value: "Sem catégorias" }]);
+  const [ isOpenModal, setIsOpenModal ] = useState(false);
   const { addProduct } = useProductsContext();
 
   const initialValues: ProductData = constants.initialProductValues;
@@ -22,150 +24,163 @@ export function DashboardProductForm() {
     validationSchema: ProductValidateSchema,
     onSubmit: values => {
       const categories = formatCategories(category);
+      setIsOpenModal(true);
       addProduct({...values, category: categories });
     },
   });
 
   return (
-    <DashboardProductFormWrapper onSubmit={formik.handleSubmit}>
-      <section>
-        <div>
-          <ProductInput
-            type="text"
-            id="title"
-            name="title"
-            label="Nome"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.title}
-            isTextArea={false}
+    <Fragment>
+       {
+        isOpenModal &&  (
+          <DashboardProductModal 
+            title="Novo produto adicionado."
+            message="Todos os dados já estão disponíveis para visualização."
+            isSuccess={true}
+            setIsOpen={setIsOpenModal}
           />
-          {formik.touched.title && formik.errors.title ? <p>{formik.errors.title}</p> : null}
-        </div>
-        <article>
+        )
+      }
+      <DashboardProductFormWrapper onSubmit={formik.handleSubmit}>
+        <section>
           <div>
             <ProductInput
-              id="value"
-              name="value"
-              label="Valor"
+              type="text"
+              id="title"
+              name="title"
+              label="Nome"
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              value={formik.values.value}
+              value={formik.values.title}
               isTextArea={false}
-              type="number"
             />
-            {formik.touched.value && formik.errors.value ? <p>{formik.errors.value}</p> : null}
+            {formik.touched.title && formik.errors.title ? <p>{formik.errors.title}</p> : null}
           </div>
+          <article>
+            <div>
+              <ProductInput
+                id="value"
+                name="value"
+                label="Valor"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.value}
+                isTextArea={false}
+                type="number"
+              />
+              {formik.touched.value && formik.errors.value ? <p>{formik.errors.value}</p> : null}
+            </div>
+            <div>
+              <ProductInput
+                id="height"
+                name="height"
+                label="Altura"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.height}
+                isTextArea={false}
+                type="number"
+              />
+              {formik.touched.height && formik.errors.height ? <p>{formik.errors.height}</p> : null}
+            </div>
+            <div>
+              <ProductInput
+                id="width"
+                name="width"
+                label="Largura"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.width}
+                isTextArea={false}
+                type="number"
+              />
+              {formik.touched.width && formik.errors.width ? <p>{formik.errors.width}</p> : null}
+            </div>
+            <div>
+              <ProductInput
+                id="length"
+                name="length"
+                label="Comprimento"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.length}
+                isTextArea={false}
+                type="number"
+              />
+              {formik.touched.length && formik.errors.length ? <p>{formik.errors.length}</p> : null}
+            </div>
+          </article>
           <div>
             <ProductInput
-              id="height"
-              name="height"
-              label="Altura"
+              id="image"
+              name="image"
+              label="Link para imagem do produto"
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              value={formik.values.height}
+              value={formik.values.image}
               isTextArea={false}
-              type="number"
+              type="text"
             />
-            {formik.touched.height && formik.errors.height ? <p>{formik.errors.height}</p> : null}
+            {formik.touched.image && formik.errors.image ? <p>{formik.errors.image}</p> : null}
           </div>
           <div>
-            <ProductInput
-              id="width"
-              name="width"
-              label="Largura"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.width}
-              isTextArea={false}
-              type="number"
+            <ProductMultipleInputValue 
+              state={category}
+              setState={setCategory}
+              name="category"
+              label="Catégorias"
             />
-            {formik.touched.width && formik.errors.width ? <p>{formik.errors.width}</p> : null}
           </div>
-          <div>
-            <ProductInput
-              id="length"
-              name="length"
-              label="Comprimento"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.length}
-              isTextArea={false}
-              type="number"
-            />
-            {formik.touched.length && formik.errors.length ? <p>{formik.errors.length}</p> : null}
-          </div>
-        </article>
-        <div>
-          <ProductInput
-            id="image"
-            name="image"
-            label="Link para imagem do produto"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.image}
-            isTextArea={false}
-            type="text"
-          />
-          {formik.touched.image && formik.errors.image ? <p>{formik.errors.image}</p> : null}
-        </div>
-        <div>
-          <ProductMultipleInputValue 
-            state={category}
-            setState={setCategory}
-            name="category"
-            label="Catégorias"
-          />
-        </div>
-      </section>
+        </section>
 
-      <section>
-        <div>
-          <ProductInput
-            id="acquisition"
-            name="acquisition"
-            label="Aquirido em"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.acquisition}
-            isTextArea={false}
-            type="date"
-          />
-          {formik.touched.acquisition && formik.errors.acquisition ? <p>{formik.errors.acquisition}</p> : null}
-        </div>
-        <div>
-          <ProductInput
-            id="barCode"
-            name="barCode"
-            label="Código de barras"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.barCode}
-            isTextArea={false}
-            type="text"
-          />
-          {formik.touched.barCode && formik.errors.barCode ? <p>{formik.errors.barCode}</p> : null}
-        </div>
-        <div>
-          <ProductInput
-            id="description"
-            name="description"
-            label="Descrição"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.description}
-            isTextArea={true}
-            type="text"
-          />
-          {formik.touched.description && formik.errors.description ? <p>{formik.errors.description}</p> : null}
-        </div>
-      </section>
-      <button 
-        type="submit" 
-        id="form-submit-button"
-      >
-        Adicionar
-      </button>
-    </DashboardProductFormWrapper>
+        <section>
+          <div>
+            <ProductInput
+              id="acquisition"
+              name="acquisition"
+              label="Aquirido em"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.acquisition}
+              isTextArea={false}
+              type="date"
+            />
+            {formik.touched.acquisition && formik.errors.acquisition ? <p>{formik.errors.acquisition}</p> : null}
+          </div>
+          <div>
+            <ProductInput
+              id="barCode"
+              name="barCode"
+              label="Código de barras"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.barCode}
+              isTextArea={false}
+              type="text"
+            />
+            {formik.touched.barCode && formik.errors.barCode ? <p>{formik.errors.barCode}</p> : null}
+          </div>
+          <div>
+            <ProductInput
+              id="description"
+              name="description"
+              label="Descrição"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.description}
+              isTextArea={true}
+              type="text"
+            />
+            {formik.touched.description && formik.errors.description ? <p>{formik.errors.description}</p> : null}
+          </div>
+        </section>
+        <button 
+          type="submit" 
+          id="form-submit-button"
+        >
+          Adicionar
+        </button>
+      </DashboardProductFormWrapper>
+    </Fragment>
   )
 }
