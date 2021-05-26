a7f7bdd17a160a97f20ee936cdb31d798c13dd6f
edmilson-dk <droidvidaboa@gmail.com>
2021-05-26 12:16:42 -0300

    feat: added news constabv8~534aaaADA

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,14 @@
 export const constants = {
   storagedProductKey: "@stored-product-key",
   storagedProducEventsKey: "@stored-product-events-key",
+  initialProductValues: {
+    title: "", 
+    description: "", 
+    height: 0, width: 0, length: 0, weight: 0, 
+    barCode: "",
+    category: [], 
+    value: 0, 
+    acquisition: "", 
+    image: "",
+  }
 }
