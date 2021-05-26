c3e0719bd25b3be346e0b7abd351963745e9f225
edmilson-dk <droidvidaboa@gmail.com>
2021-05-26 19:53:38 -0300

    feat: added news components

--- src/screens/dashboard-create/index.tsx
+++ src/screens/dashboard-create/index.tsx
@@ -1,9 +1,15 @@
 import { DashboardProductForm } from "src/components/dashboard-product-form";
+import { Container } from "src/styles/components/container";
+import { TitlePrimary } from "src/styles/components/titles";
+import { DashboardCreateWrapper } from "src/styles/screens/dashboard-create";
 
 export function DashboardCreate() {
   return (
-    <>
-      <DashboardProductForm />
-    </>
+    <DashboardCreateWrapper>
+      <Container>
+        <TitlePrimary>Adicionar novo produto</TitlePrimary>
+        <DashboardProductForm />
+      </Container>
+    </DashboardCreateWrapper>
   )
 }
