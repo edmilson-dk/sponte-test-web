f5c66f1cf76070ce7fcac946cb00dc4ded794409
edmilson-dk <droidvidaboa@gmail.com>
2021-05-25 22:49:57 -0300

    refactor: removed console.log

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -2,6 +2,5 @@ import { format, parseISO } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 export function formatDate(date: string) {
-  console.log(date)
   return format(parseISO(date), 'd MMM yy', { locale: ptBR });
 }
