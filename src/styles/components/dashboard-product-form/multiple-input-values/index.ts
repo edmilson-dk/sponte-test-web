534dcc9e4c3694e624dd3e37d6c507c2c8a7b650
edmilson-dk <droidvidaboa@gmail.com>
2021-05-26 16:12:06 -0300

    style: created dashboard product form multiple input values component styles

---
+++ src/styles/components/dashboard-product-form/multiple-input-values/index.ts
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+
+export const ProductMultipleInputValuesWrapper = styled.div`
+  width: 100%;
+
+  > div {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 10px;
+  }
+
+  > label {
+    font-size: 1.125rem;
+    font-weight: 500;
+    color: ${({ theme }) => theme.colors.gray800};
+    text-align: left;
+  }
+
+  > div input {
+    width: calc(100% - 90px);
+    height: 50px;
+    padding: 0 1rem;
+    border-radius: ${({ theme }) => theme.utility.radius};
+    background-color: ${({ theme }) => theme.colors.whiteSecond};
+    border: 1px solid ${({ theme }) => theme.colors.gray100};
+    color: ${({ theme }) => theme.colors.gray500};
+    font-size: 1.125rem;
+    font-weight: 400;
+    box-shadow: 3px 3px 3px rgba(0,0,0,0.060);
+  }
+
+  > div button {
+    width: 80px;
+    height: 50px;
+    font-size: 0;
+    border-radius: ${({ theme }) => theme.utility.radius};
+    background-color: ${({ theme }) => theme.colors.primary};
+
+    > span svg {
+      width: 30px;
+      height: 30px;
+      stroke: ${({ theme }) => theme.colors.white};
+    }
+  }
+
+  > ul {
+    width: 100%;
+    display: flex;
+    gap: 10px;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    margin-top: 20px;
+    padding: 1rem;
+
+    border-radius: ${({ theme }) => theme.utility.radius};
+    background-color: ${({ theme }) => theme.colors.whiteSecond};
+  }
+
+  > ul li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  > ul li span {
+    font-size: 1rem;
+    font-weight: 500;
+    color: ${({ theme }) => theme.colors.gray500};
+    text-align: left;
+  }
+
+  > ul li button {
+    background-color: transparent;
+    
+    &,
+    > span svg {
+      width: 30px;
+      height: 30px;
+    }
+
+    > span svg {
+      stroke: ${({ theme }) => theme.colors.danger};
+    }
+  }
+`;
