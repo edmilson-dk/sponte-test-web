1654e05f5b57a95f8a57381b5feff983882164c6
edmilson-dk <droidvidaboa@gmail.com>
2021-05-28 20:38:21 -0300

    feat: added CustomThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+import { ThemeProvider } from "styled-components";
+import { CustomThemeContextProvider } from "./contexts/CustomThemeContext";
 import { ProductsContextProvider } from "./contexts/products-context";
+import { useCustomTheme } from "./hooks/useCustomTheme";
 import { Routes } from "./routes";
-import { CustomThemeProvider } from "./styles";
+import { lightTheme } from "./styles";
 import { GlobalsStyles } from "./styles/globals";
 
 function App() {
+  const [theme, setTheme] = useCustomTheme("@product-flow-theme", lightTheme);
+
   return (
-    <CustomThemeProvider>
-      <ProductsContextProvider>
-        <GlobalsStyles />
-        <Routes />
-      </ProductsContextProvider>
-    </CustomThemeProvider>
+    <CustomThemeContextProvider theme={theme} setTheme={setTheme}>
+      <ThemeProvider theme={theme}>
+        <ProductsContextProvider>
+          <GlobalsStyles />
+          <Routes />
+        </ProductsContextProvider>
+      </ThemeProvider>
+    </CustomThemeContextProvider>
   );
 }
 
